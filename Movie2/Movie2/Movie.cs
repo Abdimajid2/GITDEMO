d9e7ef03cee69edea6d4c8b3d323486a0e9d0a42
Abdimajid2 <147602028+Abdimajid2@users.noreply.github.com>
2023-10-11 13:51:24 +0200

    uppdate

---
+++ Movie2/Movie2/Movie.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie2
+{
+    internal class Movie
+    {
+        public string MovieName;
+        public string Director { get; set; }
+
+        public DateTime Realisedate { get; set; }
+
+        public int LenghtInMinutes { get; set; }
+
+        public bool SuitableForChildren { get; set; }
+
+        public string[] Stars { get; set; }
+
+
+        public Movie(string moviename, string director, DateTime realisedate, int lenghtinminutes, bool suitableforkids, string[] stars)
+        {
+            MovieName = moviename;
+            Director = director;
+            Realisedate = realisedate;
+            LenghtInMinutes = lenghtinminutes;
+            SuitableForChildren = suitableforkids;
+            Stars = stars;
+
+
+
+
+
+
+
+        }
+
+       
+        public static void MakingMovie(Movie movie)
+        {
+            Console.Write(movie.MovieName + " är en film som har regisserats av " + movie.Director
+                              + " med premiär " + movie.Realisedate.ToString("d")
+                              + " och är " + movie.LenghtInMinutes + " minuter lång "
+                              + ", skådespelarna i filmen är");
+
+            for (int i = 0; i < movie.Stars.Length; i++)
+            {
+                if (i < movie.Stars.Length - 1)
+                {
+                    Console.Write(movie.Stars[i] + " , ");
+                }
+                else
+                {
+                    Console.Write(movie.Stars[i] + " . ");
+                }
+            }
+
+            Console.WriteLine("filmen är " + (movie.SuitableForChildren ? " lämlig för barn" : " inte lämplig för barn "));
+
+        }
+
+    }
+
+}
---
+++ Movie2/Movie2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Movie2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stars1 = { "kalle", "ahmed", "ek" };
+            Movie movie0 = new Movie("pippi", " majid", new DateTime(1999,06,28), 74, false, stars1 );
+
+            string[] stars2 = { "moahmmed", "aleksander", "sebbe" };
+            Movie movie1 = new Movie("spiderman", " mattias", new DateTime(1946, 12 , 23), 85, true, stars2);
+
+            string[] stars3 = { "salle", "sandra", "erika" };
+            Movie movie2 = new Movie("No Hasta Manyana ", " dree low", new DateTime(1875, 01, 30), 106, false, stars3);
+
+
+            List<Movie> movies = new List<Movie>
+             {
+                 movie0, movie1, movie2
+             };
+
+
+            foreach(Movie movie in movies)
+            {
+                Movie.MakingMovie(movie);
+                Console.ReadKey();
+            }
+
+           
+
+        }
+    }
+}
